77f7759441b4ac0743295de81deec1ad0e86f1e2
amilanesi <amilanesi@sparkdigital.com>
2020-04-06 12:17:39 -0300

    Pushing latest changes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,45 +23,54 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+it("Commands", () => {
 
-Cypress.Commands.add('loginToApp', ({username, password}) => {
-    cy.visit("/login");
-    cy.get('#login-email').type(username);
-    cy.get('#login-pass').type(password, {log: false});
-    cy.get('.btn').debug().click();
-  });
+    cy.fixture("column6").as("data");
 
-Cypress.Commands.add('createAdvertiser', ({name, contactName, contactEmail, phoneNumber, companyAddress}) => {
-    cy.get(".j-advertiser-section").click();
-    cy.get(".sidebar-tools > .j-advertiser-add").click();
-    cy.get("#form-field-name").type(name);
-    cy.get("#form-field-contactName").type(contactName);
-    cy.get("#form-field-contactEmail").type(contactEmail);
-    cy.get("#form-field-contactPhone").type(phoneNumber);
-    cy.get("#form-field-contactAddress").type(companyAddress);
-    cy.get("#advertiser-create-and-navigate-btn").click();
-})
+    cy.get("@data").then((data) => {
 
-Cypress.Commands.add('createIO', ({name, priContactName, priContactEmail}) => {
-    cy.get(".close").click();
-    cy.get("#form-field-name").type(name);
-    cy.get("#form-field-primaryContactName").type(priContactName);
-    cy.get("#form-field-primaryContactEmail").type(priContactEmail);
-    cy.get("#insertionOrder-create-and-navigate-btn").click();
-})
+        Cypress.Commands.add('login', () => {
+            cy.visit("/login");
+            cy.get('#login-email').type(data.userEmail);
+            cy.get('#login-pass').type(data.userPass, {log: false});
+            cy.get('.btn').debug().click();
+        })
 
-Cypress.Commands.add('createCampaign', ({name, cpm}) => {
-    cy.get(".close").click();
-    cy.get("#form-field-name").type(name);
-    cy.get("#form-field-cpm").clear();
-    cy.get("#form-field-cpm").type(cpm);
-    cy.get("#j-field-container-goal > .ng-isolate-scope > .form-group > :nth-child(4) > .ng-binding").click();
-    cy.get("#campaign-create-and-navigate-btn").click();
-})
+        Cypress.Commands.add('createAdvertiser', () => {
+            cy.get(".j-advertiser-section").click();
+            cy.get(".sidebar-tools > .j-advertiser-add").click();
+            cy.get("#form-field-name").type(data.releaseVersion);
+            cy.get("#form-field-contactName").type(data.randomName);
+            cy.get("#form-field-contactEmail").type(data.email);
+            cy.get("#form-field-contactPhone").type(data.phone);
+            cy.get("#form-field-contactAddress").type(data.address);
+            cy.get("#advertiser-create-and-navigate-btn").click();
+        })
 
-Cypress.Commands.add('createTagMedia', ({name, adTag}) => {
-    cy.get(".close").click();
-    cy.get("#form-field-name").type(name);
-    cy.get(".form-group-adTag > .CodeMirror > .CodeMirror-scroll").type(adTag);
-    cy.get("#media-save-btn").click();
+        Cypress.Commands.add('createIO', () => {
+            cy.get(".close").click();
+            cy.get("#form-field-name").type(data.releaseVersion);
+            cy.get("#form-field-primaryContactName").type(data.randomName);
+            cy.get("#form-field-primaryContactEmail").type(data.email);
+            cy.get("#insertionOrder-create-and-navigate-btn").click();
+        })
+
+        Cypress.Commands.add('createCampaign', () => {
+            cy.get(".close").click();
+            cy.get("#form-field-name").type(data.releaseVersion);
+            cy.get("#form-field-cpm").clear();
+            cy.get("#form-field-cpm").type(data.campaignCPM);
+            cy.get("#j-field-container-goal > .ng-isolate-scope > .form-group > :nth-child(4) > .ng-binding").click();
+            cy.get("#campaign-create-and-navigate-btn").click();
+        })
+
+        Cypress.Commands.add('createTagMedia', () => {
+            cy.get(".close").click();
+            cy.get("#form-field-name").type(data.releaseVersion);
+            cy.get(".form-group-adTag > .CodeMirror > .CodeMirror-scroll").type(data.mediaTag);
+            cy.get("#media-save-btn").click();
+        })
+
+    })
+
 })
