74758caf4f8cf25c68ed50acfd22340ef6f165fa
amilanesi <amilanesi@sparkdigital.com>
2020-05-05 14:54:13 -0300

    This commit fixes the issue

--- cypress/integration/Column6/newDashboard.spec.js
+++ cypress/integration/Column6/newDashboard.spec.js
@@ -5,7 +5,7 @@ describe("New Dashboard testing", () => {
     it("DemandFlow", () => {
 
         cy.visit("http://localhost:3000")
-        cy.get('.MuiContainer-root > :nth-child(1)').should("contain.text", "Route not found or under construction OOPS!")
+        cy.get('.MuiContainer-root > :nth-child(1)').should("contain.text", "Route not found or under construction")
     })
 
 })
